53e047ab6d22b3565f56888aef767070a954e353
MonkaSztuder <116058964+MonkaSztuder@users.noreply.github.com>
2023-10-19 18:30:53 +0200

    entropy, conditional entr. and information gain

--- src/snumDT.py
+++ src/snumDT.py
@@ -20,12 +20,14 @@ def create_dict(rows,key='Name'):
 def create_table(data):
     table = []
     for key, value in data.items():
-        table.append(value/100)
+        table.append(value/len(data.values()))
     return table
     
 def entropy(probabilities):
     entropy = 0
     for p in probabilities:
+        if p == 1 or p==0:
+            return 0
         if p != 0:
             entropy += p * math.log2(p)
     return -entropy
@@ -36,39 +38,14 @@ def conditional_entropy(entropies,probabilities):
         conditionalentropy += probabilities[p] * entropies[p]
     return conditionalentropy
 
-def information_gain(parent, children):
-    parent_entropy = entropy(parent)
-    # print(f"Parent entropy: {parent_entropy}")
-    children_entropy = 0
-    for child in children:
-        for i in range(len(child)):
-            child[i] =child[i]/100
-            
-        children_entropy += entropy(child)
-    # print(f"Children entropy: {children_entropy}")
-    return parent_entropy - children_entropy
-
+def information_gain(parententropy, childrenentropy):
+    return  parententropy - childrenentropy
+    
 
 def print_dict(data):
     for key, value in data.items():
         print(f"{key}: {value}")
 
-
-def get_best_attribute(data): #naprawic
-    
-    best_attribute = None
-    best_entropy = float('inf')
-    for key in data[0].keys():
-        if key == 'Name' or key == 'Survived' or key == 'PassengerId':
-            continue
-        attribute_dict = create_dict(data, key)
-        attribute_table = create_table(attribute_dict)
-        ig = information_gain(attribute_table, [attribute_table, [1 - x for x in attribute_table]])
-        if ig < best_entropy:
-            best_entropy = ig
-            best_attribute = key
-        print(f"Information gain for {key} is {ig}")
-    return best_attribute
 
 def remove_attribute(data, attribute):
     for row in data:
@@ -98,12 +75,25 @@ def get_decision_prob(list, decision_attr):
         probabilities.append(count / len(list))
 
     return probabilities
+
+def get_yes_no_prob(list, decision_attr,datacount):
+    decision_classes = set([row[decision_attr] for row in list])
+    decisions = []
+
+    for decision_class in decision_classes:
+        count = sum(1 for row in list if row[decision_attr] == decision_class)
+        decisions.append(count/datacount)
+
+    return decisions
+
+
     
 
 def main():
-    TitanicData=read_csv_file('data\\titanic-homework.csv')
+    TitanicData=read_csv_file('data\\LabExe.csv')
+    # TitanicData=read_csv_file('data\\titanic-homework.csv')
 
-    for i in range(100):
+    for i in range(len(TitanicData)):
         if TitanicData[i]['Age'] <='20':
             TitanicData[i]['Age'] = 'young'
             continue
@@ -112,21 +102,27 @@ def main():
             continue
         TitanicData[i]['Age'] = 'old'
 
-    mainentropy = entropy(create_table(create_dict(TitanicData,'Survived')))
-    print("Main entropy: ",mainentropy)
+    mainentropy = entropy(get_decision_prob(TitanicData, "Survived"))
+    print("Main entropy: ",mainentropy," Probabiltes: ",get_decision_prob(TitanicData, "Survived"))
 
     for key in TitanicData[0].keys():
         if key == 'Name' or key == 'Survived' or key == 'PassengerId':
             continue
+        # if key == 'Sex':
         lists = split_list_by_attr(TitanicData, key)
-        print(key+" entropy:")
-        entropies=[]
+        print(key+":")
+        entropiesforkey=[]
+        decistions = []
         for a in lists:
-            print(a[0], get_decision_prob(a[1], "Survived"))
-            print(a[0],entropy(get_decision_prob(a[1], "Survived")))
-            entropies.append(entropy(get_decision_prob(a[1], "Survived")))
-        # print("Conditional entropy: ")
-        # print(conditional_entropy(entropies,))
+            # print(a[0], get_decision_prob(a[1], "Survived"))
+            # print(a[0],entropy(get_decision_prob(a[1], "Survived")))
+            entropiesforkey.append(entropy(get_decision_prob(a[1], "Survived")))
+            for dec in get_yes_no_prob(a[1], key,len(TitanicData)):
+                decistions.append(dec)
+        print("Entropy: ",entropiesforkey)
+        print("Decisions: ",decistions)
+        print("Conditional entropy: ",conditional_entropy(entropiesforkey,decistions))
+        print("Information gain for ",key,": ",information_gain(mainentropy,conditional_entropy(entropiesforkey,decistions)),"\n")
 
     # for i in range(5):
     #     best=get_best_attribute(TitanicData)
@@ -134,6 +130,22 @@ def main():
     #     # print_dict(create_dict(TitanicData,best))
     #     remove_attribute(TitanicData,best)
     
+''' dane dla 10 pierwszych rekordów (są one w tym LabExe.csv)
+Age[young,middle,old]
+Age[2,7,1]
+Age[yes:1 no:1 ; yes:4 no:3; yes:0 no:1] 
 
+PassengerId,Pclass,Name,Sex,Age,SibSp,Parch,Survived
+1,3,"Braund, Mr. Owen Harris",male,22,1,0,0
+2,1,"Cumings, Mrs. John Bradley (Florence Briggs Thayer)",female,38,1,0,1
+3,3,"Heikkinen, Miss. Laina",female,26,0,0,1
+4,1,"Futrelle, Mrs. Jacques Heath (Lily May Peel)",female,35,1,0,1
+5,3,"Allen, Mr. William Henry",male,35,0,0,0
+6,3,"Moran, Mr. James",male,34,0,0,0
+7,1,"McCarthy, Mr. Timothy J",male,54,0,0,0
+8,3,"Palsson, Master. Gosta Leonard",male,2,3,1,0
+9,3,"Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)",female,27,0,2,1
+10,2,"Nasser, Mrs. Nicholas (Adele Achem)",female,14,1,0,1
+'''
 
 main()
